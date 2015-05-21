f498289eada93aef3cefdc07d02bbc4035974a27
Paul Garaud <circld@users.noreply.github.com>
2015-05-20 19:13:33 -0600

    refactor app controller, remove api key

---
+++ README.md
@@ -0,0 +1,4 @@
+# DenverCrimeMap
+Animated heatmap of Denver crime for the month of March 2015
+
+See it in action [here](http://circld.pythonanywhere.com).
---
+++ denver_crime_map.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from os.path import join as path_join
+from urllib2 import urlopen
+
+
+app = Flask(__name__)
+local_path = 'file://' + app.root_path
+file_name = 'crime_cats_201503'
+data_file = path_join('/static', 'data', '{}.topojson'.format(file_name))
+text_file = '/static/write-up.txt'
+with open('leaflet_token', 'r') as f:
+    leaflet_token = f.read().strip()
+
+
+@app.route('/')
+def heatmap_page(name=None):
+    return render_template('proof-of-concept.html', token = leaflet_token,
+                           data=data_file, info_text = text_file, fname=file_name)
+
+
+if __name__ == '__main__':
+    app.run()
